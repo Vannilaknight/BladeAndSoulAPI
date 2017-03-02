390a7a732e81bd07d1a030e6cb755918801a45a6
Taylor Boyd <a6pgjzz@usac.mmm.com>
2017-03-02 13:01:01 -0700

    changed a fix

--- public/app/main/mainCtrl.js
+++ public/app/main/mainCtrl.js
@@ -1,14 +1,5 @@
 angular.module('app').controller('mainCtrl', function ($scope, $http, $rootScope, $interval, $location) {
-  $scope.searchResults = {
-    "username": "username",
-    "name": "character name",
-    "class": "Blade Master",
-    "level": 50,
-    "hongmoonLevel": 2,
-    "server": "Mushin",
-    "faction": "Cerulean Order Recruit",
-    "guild": ""
-  };
+  $scope.searchResults = {};
 
   $scope.search = function () {
     $http.get("/api/character/" + $scope.characterName).then(function (results) {
